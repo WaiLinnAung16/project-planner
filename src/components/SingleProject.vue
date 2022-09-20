e8a8874f785f9b776332f6ff41a299199faa9059
WaiLinnAung18 <waia00198@gmail.com>
2022-09-20 16:17:07 +0630

    project-planner

---
+++ src/components/SingleProject.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="project" @click="showDetail!=showDetail">
+    <h3>{{project.title}}</h3>
+    <p v-if="showDetail">{{project.detail}}</p>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "SingleProject.vue",
+  props:["project"],
+  data(){
+    return{
+      showDetail:false
+    }
+  },
+}
+</script>
+
+<style scoped>
+  .project{
+    background-color: #f6f6f6;
+    padding: 10px;
+    margin: 10px 0;
+    cursor:pointer;
+  }
+  .project h3{
+    color: #0f6daa;
+  }
+</style>
